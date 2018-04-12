a9329741dccf99c021e05f386f95b35b139cf859
tylerlove <tml.335@gmail.com>
2018-04-12 20:20:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,11 @@
 def fizzbuzz(num)
   if num % 3 && num % 5 == 0
     return "Fizzbuzz"
-
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else
+    return nil
+  end
 end
